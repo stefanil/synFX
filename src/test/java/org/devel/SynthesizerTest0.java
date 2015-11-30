80b4e79eeb42a4299bfe1da9181f84b726bb280e
Stefan Illgen <stefan.illgen@saxsys.de>
2015-11-30 16:02:52 +0100

    pom enhanced; bitch bend example; note on/off example

---
+++ src/test/java/org/devel/SynthesizerTest0.java
@@ -0,0 +1,29 @@
+package org.devel;
+
+import javax.sound.midi.*;
+
+/**
+ * Created by stefan.illgen on 30.11.2015.
+ */
+public class SynthesizerTest0 {
+
+    public static final int MAX_PITCH_BEND = 16383;
+    private static final int CENTER_PITCH_BEND = 8192;
+    private static final int MIN_PITCH_BEND = 0;
+
+    public static void main(String[] args) throws MidiUnavailableException, InterruptedException {
+        Synthesizer synth = MidiSystem.getSynthesizer();
+        synth.open();
+
+        final MidiChannel[] mc = synth.getChannels();
+        mc[1].noteOn(40,600);
+
+        mc[1].setPitchBend(MAX_PITCH_BEND);
+        Thread.sleep( 2000 );
+        mc[1].setPitchBend(MIN_PITCH_BEND);
+        Thread.sleep( 2000 );
+        mc[1].setPitchBend(CENTER_PITCH_BEND);
+        Thread.sleep( 2000 );
+    }
+
+}
